a315494917fa9504da8fe688816d73f6e3d18b24
tania <natania.william09@gmail.com>
2024-08-05 14:23:20 +0700

    feat: create register page

---
+++ apps/web/src/app/register/page.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+interface IRegisterPageProps {
+}
+
+const RegisterPage: React.FunctionComponent<IRegisterPageProps> = (props) => {
+  return (
+    <div>
+        <h1> Register Page </h1>
+    </div>
+  );
+};
+
+export default RegisterPage;
